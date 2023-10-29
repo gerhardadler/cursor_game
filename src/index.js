2553d65ac936ff13a8e53dd21dc81d8441d65a22
gerhardadler <gerhardskartveit@gmail.com>
2023-10-29 23:51:07 +0100

    can now build correctly

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 import Coordinator from "./scripts/coordinator.js";
+import "./css/index.css";
+
 const coordinator = new Coordinator(document, document.body);
--- webpack.common.js
+++ webpack.common.js
@@ -1,12 +1,13 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
   entry: "./src/index.js",
-  devtool: "inline-source-map",
-  devServer: {
-    static: "./dist",
-  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+  ],
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
@@ -14,6 +15,11 @@ module.exports = {
   },
   module: {
     rules: [
+      {
+        test: /\.css$/i,
+
+        use: ["style-loader", "css-loader"],
+      },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
